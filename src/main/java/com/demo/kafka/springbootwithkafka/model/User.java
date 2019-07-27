4e66d19ec24228c1fed9b5c1f1dae2cc56711f8f
venkaiah <venki552@gmail.com>
2019-07-27 15:30:30 +0530

    initial commit

---
+++ src/main/java/com/demo/kafka/springbootwithkafka/model/User.java
@@ -0,0 +1,13 @@
+package com.demo.kafka.springbootwithkafka.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class User {
+
+	private String name;
+	private int age;
+
+}
